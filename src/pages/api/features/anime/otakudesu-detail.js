78de35dc904f6a941e893c9948983fc85570396b
[RyHarJR] <[ryhargans01@gmail.com]>
2025-06-24 15:22:14 +0700

    feat: add otakudesu detail API handler for fetching anime information

---
+++ src/pages/api/features/anime/otakudesu-detail.js
@@ -0,0 +1,32 @@
+import otakudesu from "../../../../../lib/scrapers/otakudesu"
+import checkApiKey from '../../../../../lib/middleware/checkApikey';
+import runMiddleware from '../../../../../lib/runMiddleware';
+
+const handler = async (req, res) => {
+    if (req.method !== 'GET') return res.status(405).end();
+    try {
+        await runMiddleware(req, res, checkApiKey);
+        const { url } = req.query;
+        const response = await otakudesu.detail(url);
+        return res.status(200).json({
+            status: true,
+            message: 'Success',
+            result: response
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: false,
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+handler.method = 'GET';
+handler.folder = 'anime';
+handler.desc = 'Get Detail Anime Info from Otakudesu';
+handler.query = "url";
+handler.example = "?url=https://otakudesu.cloud/anime/compass-20-sub-indo";
+handler.status = true;
+
+export default handler
