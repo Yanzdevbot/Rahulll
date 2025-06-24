32744a623f9f6b5ee0b6a548fafdada82cbb05aa
[RyHarJR] <[ryhargans01@gmail.com]>
2025-06-24 15:22:43 +0700

    feat: implement otakudesu search handler for fetching anime information

---
+++ src/pages/api/features/anime/otakudesu-search.js
@@ -0,0 +1,32 @@
+import otakudesu from "../../../../../lib/scrapers/otakudesu"
+import checkApiKey from '../../../../../lib/middleware/checkApikey';
+import runMiddleware from '../../../../../lib/runMiddleware';
+
+const handler = async (req, res) => {
+    if (req.method !== 'GET') return res.status(405).end();
+    try {
+        await runMiddleware(req, res, checkApiKey);
+        const { query } = req.query;
+        const response = await otakudesu.search(query);
+        return res.status(200).json({
+            status: true,
+            message: 'Success',
+            result: response
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            status: false,
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+handler.method = 'GET';
+handler.folder = 'anime';
+handler.desc = 'Search Anime Info from Otakudesu';
+handler.query = "query";
+handler.example = "?query=one%20piece";
+handler.status = true;
+
+export default handler
