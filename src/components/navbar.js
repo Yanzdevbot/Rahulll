d1748a14d2166c69ee765cc368703148ea6a0a1c
[RyHarJR] <[ryhargans01@gmail.com]>
2025-06-24 21:42:56 +0700

    fix: update logout callback URL and dashboard link in Navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -51,7 +51,7 @@ export default function Navbar() {
     }, [session]);
 
     const handleLogout = async () => {
-        await signOut({ callbackUrl: '/user/dashboard' })
+        await signOut({ callbackUrl: '/dashboard' })
     }
 
     return (
@@ -128,7 +128,7 @@ const mainPage = [
     {
         title: "Dashboard",
         icon: "m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25",
-        link: "/user/dashboard"
+        link: "/dashboard"
     },
     {
         title: "Pricing",
