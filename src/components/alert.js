207d1f5adeb069157cb8009f95c73836d796104b
[RyHarJR] <[ryhargans01@gmail.com]>
2025-06-27 23:56:16 +0700

    style: update text color in Alert component for better visibility

--- src/components/alert.js
+++ src/components/alert.js
@@ -5,11 +5,11 @@ export default function Alert({ message, visible, onClose }) {
 
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-            <div className="bg-white dark:bg-gray-800 text-black dark:text-white p-4 rounded-xl shadow-xl w-full max-w-sm flex flex-col items-center m-10">
+            <div className="bg-white dark:bg-gray-800 text-black dark:p-4 rounded-xl shadow-xl w-full max-w-sm flex flex-col items-center m-10">
                 <p className="mb-4">{message}</p>
                 <button
                     onClick={onClose}
-                    className="px-4 py-2 bg-[#483AA0] hover:bg-[#372a7a] hover:scale-105 active:scale-95 text-white rounded transition duration-300"
+                    className="px-4 py-2 bg-[#483AA0] hover:bg-[#372a7a] hover:scale-105 active:scale-95 rounded transition duration-300"
                 >
                 OK
                 </button>
