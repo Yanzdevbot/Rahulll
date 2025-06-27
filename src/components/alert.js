793c622a8a0b07c02ce1deee1d3a0a2821908075
[RyHarJR] <[ryhargans01@gmail.com]>
2025-06-27 18:51:02 +0700

    style: update Alert component structure and button styles for improved consistency and user experience

--- src/components/alert.js
+++ src/components/alert.js
@@ -4,18 +4,16 @@ export default function Alert({ message, visible, onClose }) {
     if (!visible) return null;
 
     return (
-        <alert>
-            <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-                <div className="bg-white dark:bg-gray-800 text-black dark:text-white p-4 rounded-xl shadow-xl w-full max-w-sm flex flex-col items-center m-10">
-                    <p className="mb-4">{message}</p>
-                    <button
-                        onClick={onClose}
-                        className="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition"
-                    >
-                    OK
-                    </button>
-                </div>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+            <div className="bg-white dark:bg-gray-800 text-black dark:text-white p-4 rounded-xl shadow-xl w-full max-w-sm flex flex-col items-center m-10">
+                <p className="mb-4">{message}</p>
+                <button
+                    onClick={onClose}
+                    className="px-4 py-2 bg-[#483AA0] hover:bg-[#372a7a] hover:scale-105 active:scale-95 text-white rounded transition duration-300"
+                >
+                OK
+                </button>
             </div>
-        </alert>
+        </div>
     );
 }
