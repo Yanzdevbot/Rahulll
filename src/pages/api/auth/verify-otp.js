44fa895700294efea0d1105b5ed9b77daad472db
[RyHarJR] <[ryhargans01@gmail.com]>
2025-06-29 01:08:38 +0700

    fix: remove unnecessary space in user save operation

--- src/pages/api/auth/verify-otp.js
+++ src/pages/api/auth/verify-otp.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
             createdAt: new Date()
         });
 
-        await newUser .save();
+        await newUser.save();
 
         return res.status(201).json({ message: 'Akun berhasil dibuat' });
     } catch (error) {
